d53120e8d60a8b834b7ee7f08dbc7f37a26d3039
Can Bayar <canbayar91@gmail.com>
2020-09-29 22:58:32 +0300

    Initial commit

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountdownTimer : MonoBehaviour {
+
+	public float timer;
+	public float timerLimit = 100.0f;
+
+	public GUIText timerText;
+	private FinishLine finishLine;
+
+	// Use this for initialization
+	void Start () {
+		timer = timerLimit;
+		SetTimerText ();
+
+		finishLine = GameObject.Find ("FinishLine").GetComponent<FinishLine>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		SetTimerText ();
+		if (timer <= 0.0f) {
+			finishLine.resultText.text = "YOU LOST!";
+			finishLine.ResetLevel ();
+			timer = timerLimit;
+		}
+	}
+
+	void SetTimerText() {
+		timerText.text = "Time Left: " + timer.ToString("f0");
+	}
+
+}
---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishLine : MonoBehaviour {
+
+	public GUIText resultText;
+
+	// Use this for initialization
+	void Start () {
+		resultText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Called when the object is triggered
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "boulder") {
+			resultText.text = "YOU WON!";
+			ResetLevel ();
+		}
+	}
+
+	public void ResetLevel() {
+		Application.LoadLevel (0);
+	}
+
+}
